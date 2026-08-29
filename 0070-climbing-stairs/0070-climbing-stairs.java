cb546b7459baac057ff1c566c361a546a835da41
harshrajgupta-spec <rajh98404@gmail.com>
2026-08-29 16:47:40 +0530

    Time: 0 ms (100%), Space: 41.8 MB (87.02%) - LeetHub

---
+++ 0070-climbing-stairs/0070-climbing-stairs.java
@@ -0,0 +1,20 @@
+class Solution {
+    Map<Integer, Integer> map=new HashMap<>();
+    public int climbStairsHelper(int n) {
+        if(map.containsKey(n)){
+            return map.get(n);
+        }
+        if(n==0){
+            return 1;
+        }
+        if(n<0){
+            return 0;
+        }
+        int res=climbStairsHelper(n-1)+climbStairsHelper(n-2);
+        map.put(n, res);
+        return map.get(n);
+    }
+    public int climbStairs(int n) {
+        return climbStairsHelper(n); 
+    }
+}
