4bc39f24cbd13cc53ee106260067a8c838a6c643
harshrajgupta-spec <rajh98404@gmail.com>
2026-09-14 16:06:43 +0530

    Time: 4 ms (98.34%), Space: 47.5 MB (85.02%) - LeetHub

---
+++ 0680-valid-palindrome-ii/0680-valid-palindrome-ii.java
@@ -0,0 +1,27 @@
+class Solution {
+    public boolean isPalindrome(String s, int left, int right){
+        
+        int i=left;
+        int j=right;
+        while(i<j){
+            if(s.charAt(i)!=s.charAt(j)){
+                return false;
+            }
+            i++;
+            j--;
+        }
+        return true;
+    }
+    public boolean validPalindrome(String s) {
+        int left=0;
+        int right=s.length()-1;
+        while(left<right){
+            if(s.charAt(left)!=s.charAt(right)){
+                 return isPalindrome(s, left+1, right) || isPalindrome(s, left, right-1);
+            }
+            left++;
+            right--;
+        }
+        return true;
+    }
+}
